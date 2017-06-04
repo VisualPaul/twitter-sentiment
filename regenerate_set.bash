2d10f4f852d82f24817b470d6951154c9e244f09
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-06-04 19:45:28 +0000

    fixed regenerate_set.bash

--- regenerate_set.bash
+++ regenerate_set.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python2 to_vw.py training.1600000.processed.noemoticon.csv train.vw.new testdata.manual.2009.06.14.csv test.vw -p
-rm train.vw.new *.cache
+python2 to_vw.py training.1600000.processed.noemoticon.csv train.vw testdata.manual.2009.06.14.csv test.vw -p
+rm *.cache
