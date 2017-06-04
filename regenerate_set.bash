8ab940d2862c2b0ec43c5fe558be98cb0ec9ab29
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-06-04 10:40:52 +0300

    shuffle added and progress reworked

--- regenerate_set.bash
+++ regenerate_set.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 python2 to_vw.py training.1600000.processed.noemoticon.csv train.vw.new testdata.manual.2009.06.14.csv test.vw -p
-shuf train.vw.new > train.vw
 rm train.vw.new *.cache
--- to_vw.py
+++ to_vw.py
@@ -2,8 +2,10 @@ import argparse
 import csv
 import sys
 import numpy as np
+import random
 import datetime
 import re
+import time
 from collections import Counter
 from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
 
@@ -28,7 +30,9 @@ argument_parser.add_argument('test_vw',
                              type=argparse.FileType('w'),
                              default=sys.stdout,
                              nargs='?')
+argument_parser.add_argument('--write-immediately', '-i', action='store_true')
 argument_parser.add_argument('--progress', '-p', action='store_true')
+
 args = argument_parser.parse_args()
 user_tweets = Counter()
 user_positive = Counter()
@@ -37,18 +41,30 @@ date_re = re.compile(r'(?P<week_day>\w{3}) (?P<month>\w{3}) (?P<day>\d\d) (?P<ho
 replace_pattern = re.compile(r'@\w*|[0-9]+|[a-zA-Z0-9\.\+_]+@[a-zA-Z0-9\._]+|(https?://)?[\w\d]+\.[\w\d\.]+/\S+|[:;][\-\=]*D|[xX]+D|[^a-zA-Z0-9\s@]')
 tweets = 0
 
-def csv_rows(f):
+def csv_rows(f, row_num=None):
     iters = 0
-    next_print = 1000
+    start_time = time.time()
+    nsecs = 1
     f.seek(0)
     for pol, id, date, lyx, user, tweet in csv.reader(f):
         if pol == '2':
             continue
         yield pol, id, date, lyx, user, tweet
         iters += 1
-        if iters == next_print and args.progress:
-            next_print *= 2
-            print iters, 'processed'
+        if iters % 1000 == 0 and args.progress:
+            current_time = time.time()
+            if current_time < start_time + nsecs:
+                continue
+            nsecs += 1
+            if row_num is None:
+                print '\rProcessed: {:8,d} in {:4d} seconds'.format(iters, int(round(current_time - start_time))),
+            else:
+                eta = (current_time - start_time) / float(iters) * row_num
+                print '\rProcessed: {:8,d} of {:8,d} in {:4d} seconds (ETA: {})'.format(
+                    iters, row_num, int(round(current_time - start_time)), int(round(eta - current_time + start_time))),
+            sys.stdout.flush()
+    if nsecs != 1:
+        print
 
 if args.progress:
     print 'Train pass [1/2]:'
@@ -66,8 +82,8 @@ for pol, id, date, lyx, user, tweet in csv_rows(args.csv_file):
 
 ADDED_TWEETS = 1
 
-def process_file(input_file, output_file, train):
-    for pol, id, date, lyx, user, tweet in csv_rows(input_file):
+def process_file(input_file, line_visitor, train, row_num=None):
+    for pol, id, date, lyx, user, tweet in csv_rows(input_file, row_num=row_num):
         features = []
         lbl = 1 if pol == '4' else -1
         pos = 1 if pol == '4' else 0
@@ -89,10 +105,33 @@ def process_file(input_file, output_file, train):
                 continue
             tfidf = tf[word] * np.log(float(tweets) / (df[word] + 1))
             features.append(word + ':' + str(tfidf))
-        print >> output_file, '{} {}|a'.format(lbl, id), ' '.join(features)
+        line_visitor.visit('{} {}|a '.format(lbl, id) + ' '.join(features))
+
+class PrintLineVisitor(object):
+    def __init__(self, file):
+        self.file = file
+
+    def visit(self, line):
+        print >> self.file, line
+
+class StoreLineVisitor(object):
+    def __init__(self):
+        self.list = []
+    def visit(self, line):
+        self.list.append(line)
+
 if args.progress:
     print 'Train pass [2/2]:'
-process_file(args.csv_file, args.vw_file, train=True)
+if args.write_immediately:
+    visitor = PrintLineVisitor(args.vw_file)
+else:
+    visitor = StoreLineVisitor()
+process_file(args.csv_file, visitor, train=True, row_num=tweets)
+if not args.write_immediately:
+    random.shuffle(visitor.list)
+    for line in visitor.list:
+        print >> args.vw_file, line
+del visitor
 if args.progress:
     print 'Test pass'
-process_file(args.test_csv, args.test_vw, train=False)
+process_file(args.test_csv, PrintLineVisitor(args.test_vw), train=False)
