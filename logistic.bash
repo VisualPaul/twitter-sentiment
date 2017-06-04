2f9faf7250f40c5edf2d489921fc36a95cc63478
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-06-04 22:50:12 +0300

    added glove

--- logistic.bash
+++ logistic.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source venv/bin/activate
-COMMAND="vw --quiet -b 24 -d train.vw -c --ftrl --l1 1e4 --l2 1e-4 -f model --holdout_off --loss_function logistic --link=logistic --passes 12 -q aa"
+COMMAND="vw --quiet -b 24 -d train.vw.new -c --ftrl --l1 1e4 --l2 1e-4 -f model --holdout_off --loss_function logistic --link=logistic --passes 12 -q aa"
 echo "$COMMAND"
 $COMMAND
-vw -i model -t -d test.vw -p pred.txt --quiet
+vw -i model -t -d test.vw.new -p pred.txt --quiet
 python accuracy.py pred.txt testdata.manual.2009.06.14.csv
--- to_vw.py
+++ to_vw.py
@@ -30,6 +30,9 @@ argument_parser.add_argument('test_vw',
                              type=argparse.FileType('w'),
                              default=sys.stdout,
                              nargs='?')
+argument_parser.add_argument('--vectorizer', '-v' '--vectoriser',
+                             choices=['tfidf', 'glove'],
+                             default='tfidf')
 argument_parser.add_argument('--write-immediately', '-i', action='store_true')
 argument_parser.add_argument('--progress', '-p', action='store_true')
 
@@ -40,6 +43,80 @@ df = Counter()
 date_re = re.compile(r'(?P<week_day>\w{3}) (?P<month>\w{3}) (?P<day>\d\d) (?P<hour>\d\d):(?P<minute>\d\d):(?P<second>\d\d) (?P<tz>\w{3}) (?P<year>\d{4})')
 replace_pattern = re.compile(r'@\w*|[0-9]+|[a-zA-Z0-9\.\+_]+@[a-zA-Z0-9\._]+|(https?://)?[\w\d]+\.[\w\d\.]+/\S+|[:;][\-\=]*D|[xX]+D|[^a-zA-Z0-9\s@]')
 tweets = 0
+
+class Vectorizer(object):
+    def __init__(self):
+        pass
+    def fit_tweet(self, tweet):
+        pass
+    def finalize_fit(self):
+        pass
+    def vectorize(self, tweet):
+        raise ValueError('Not implemented!')
+
+class TfIdfVectorizer(Vectorizer):
+    def __init__(self):
+        self.df = Counter()
+        self.tweets = 0
+    def fit_tweet(self, tweet):
+        for word in set(tweet):
+            if word not in stops:
+                self.df[word] += 1
+        self.tweets += 1
+    def finalize_fit(self):
+        self.tweets_f = float(self.tweets)
+    def vectorize(self, tweet):
+        tf = Counter(tweet)
+        return {word: tf[word] * np.log(self.tweets_f / (df[word] + 1))
+                for word in tf
+                if (word not in stops and df[word] < 4 )} # I am (still) a Lisp programmer
+
+class GloveVectorizer(Vectorizer):
+    def finalize_fit(self):
+        self.words_dict = {}
+        with open('glove.twitter.27B.200d.txt') as f:
+            for line in f:
+                text = line.decode('utf-8').split()
+                if len(text) != 201:
+                    continue
+                self.words_dict[text[0]] = np.array([float(x) for x in text[1:]],
+                                                     dtype='float32')
+    def vectorize(self, tweet):
+        vector = np.mean([self.words_dict[word] for word in tweet])
+        return {'_' + x: vector[x] for x in xrange(200)}
+
+
+if args.vectorizer == 'tfidf':
+    vec = TfIdfVectorizer()
+elif args.vectorizer == 'glove':
+    vec = GloveVectorizer()
+else:
+    raise ValueError('incorrect vectorizer argument: ' + repr(args.vectorizer))
+
+def replace_function(*args):
+    if len(args) % 2 != 0:
+        raise ValueError('args must be in pairs')
+    patterns = [re.compile(pat) for pat in args[0::2]]
+    replacements = args[1::2]
+    everything = zip(patterns, replacements)
+    def fun(text):
+        for pat, rep in everything:
+            text = re.sub(pat, rep, text)
+        return text
+    return fun
+
+tweet_replace = replace_function(
+    r'https?:\/\/\S+\b|www\.(\w+\.)+\S*', r' <URL> ',
+    r'@\w*', r' <USER> ',
+    r'#([A-Z0-9\-]+)', r' <HASHTAG> \1 <ALLCAPS> ',
+    r'#(\S+)', lambda x: ' <HASHTAG> ' + ' '.join(re.split(r'(?=[A-Z])', x.group(1))),
+    r'<3', r'<HEART>',
+    r'([!?.]){2,}', r' \1 <REPEAT> ',
+    r'\b(\S*?)(.)\2{2,}\b', r' \1\2 <ELONG> ',
+    r'\s([^a-z0-9()<>\'`\-]){2,}\s', lambda x: x.group(0).lower() + '<ALLCAPS> ')
+
+def tweet_to_words(tweet):
+    return [x for x in re.split(r'[^\w<>]', tweet_replace(tweet).lower()) if x != '']
 
 def csv_rows(f, row_num=None):
     iters = 0
@@ -74,13 +151,11 @@ for pol, id, date, lyx, user, tweet in csv_rows(args.csv_file):
     user_tweets[user] += 1
     if pol == '4':
         user_positive[user] += 1
-    for word in set(re.sub(replace_pattern, ' ', tweet).lower().split()):
-        if word in stops:
-            continue
-        df[word] += 1
+    vec.fit_tweet(tweet_to_words(tweet))
         
 
 ADDED_TWEETS = 1
+vec.finalize_fit()
 
 def process_file(input_file, line_visitor, train, row_num=None):
     for pol, id, date, lyx, user, tweet in csv_rows(input_file, row_num=row_num):
@@ -88,24 +163,14 @@ def process_file(input_file, line_visitor, train, row_num=None):
         lbl = 1 if pol == '4' else -1
         pos = 1 if pol == '4' else 0
         features.append('.user_tweets:' + str(user_tweets[user]))
-        if train:
-            counter = ((user_positive[user] - pos + .5 * ADDED_TWEETS)
-                       / (user_tweets[user] - 1 + ADDED_TWEETS))
-        else:
-            counter = ((user_positive[user] + .5 * ADDED_TWEETS)
-                       / (user_tweets[user] + ADDED_TWEETS))
-#        features.append('.user_counter:' + str(counter))
         date_match = re.match(date_re, date)
         assert date_match is not None
         features.append('.week_day_' + date_match.group('day'))
         features.append('.hour_' + date_match.group('hour'))
-        tf = Counter(re.sub(replace_pattern, ' ', tweet).lower().split())
-        for word in tf:
-            if word in stops:
-                continue
-            tfidf = tf[word] * np.log(float(tweets) / (df[word] + 1))
-            features.append(word + ':' + str(tfidf))
-        line_visitor.visit('{} {}|a '.format(lbl, id) + ' '.join(features))
+        tf = Counter(tweet_to_words(tweet))
+        features = vec.vectorize(tweet_to_words(tweet))
+        line_visitor.visit('{} {}|a '.format(lbl, id) + 
+            ' '.join([str(x) + ':' + str(features[x]) for x in features]))
 
 class PrintLineVisitor(object):
     def __init__(self, file):
