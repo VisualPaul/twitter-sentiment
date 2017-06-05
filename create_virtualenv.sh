18cf4c81fd23f5ecd2c1d116f6ce6ca9c75e1238
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-06-06 01:21:11 +0300

    create_virtualenv

---
+++ create_virtualenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Prerequesitest: python, pip, virtualenv + blas, gcc, etc for the numpy"
+virtualenv venv
+source venv/bin/activate
+pip install numpy pandas scipy scikit-learn gensim python-dateutil
