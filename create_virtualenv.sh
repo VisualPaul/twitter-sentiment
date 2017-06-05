10fb165bdf97c93fc9d31edee442617e2255029f
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-06-06 01:45:11 +0300

    remove dateutil install

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -2,4 +2,4 @@
 echo "Prerequesitest: python, pip, virtualenv + blas, gcc, etc for the numpy"
 virtualenv venv
 source venv/bin/activate
-pip install numpy pandas scipy scikit-learn gensim python-dateutil
+pip install numpy pandas scipy scikit-learn gensim
