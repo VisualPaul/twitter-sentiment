65205cf82add0e6018fe2c00a84fd5410404d6bf
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-06-06 01:20:54 +0300

    w2v added

--- to_vw.py
+++ to_vw.py
@@ -2,8 +2,10 @@ import argparse
 import csv
 import sys
 import numpy as np
+from gensim.models import Word2Vec
+from multiprocessing import cpu_count
 import random
-import datetime
+from dateutil.parser import parse as parse_date
 import re
 import time
 from collections import Counter
@@ -31,7 +33,7 @@ argument_parser.add_argument('test_vw',
                              default=sys.stdout,
                              nargs='?')
 argument_parser.add_argument('--vectorizer', '-v' '--vectoriser',
-                             choices=['tfidf', 'glove'],
+                             choices=['tfidf', 'glove', 'w2v'],
                              default='tfidf')
 argument_parser.add_argument('--write-immediately', '-i', action='store_true')
 argument_parser.add_argument('--progress', '-p', action='store_true')
@@ -39,10 +41,10 @@ argument_parser.add_argument('--progress', '-p', action='store_true')
 args = argument_parser.parse_args()
 user_tweets = Counter()
 user_positive = Counter()
-df = Counter()
-date_re = re.compile(r'(?P<week_day>\w{3}) (?P<month>\w{3}) (?P<day>\d\d) (?P<hour>\d\d):(?P<minute>\d\d):(?P<second>\d\d) (?P<tz>\w{3}) (?P<year>\d{4})')
+date_re = re.compile(
+    r'(?P<week_day>\w{3}) (?P<month>\w{3}) (?P<day>\d\d) (?P<hour>\d\d):(?P<minute>\d\d):(?P<second>\d\d) (?P<tz>\w{3}) (?P<year>\d{4})')
 replace_pattern = re.compile(r'@\w*|[0-9]+|[a-zA-Z0-9\.\+_]+@[a-zA-Z0-9\._]+|(https?://)?[\w\d]+\.[\w\d\.]+/\S+|[:;][\-\=]*D|[xX]+D|[^a-zA-Z0-9\s@]')
-tweets = 0
+
 if args.progress:
     def report_progress(iterable, total=None, period=1000):
         start_time = time.time()
@@ -70,34 +72,30 @@ else:
         return iterable
 
 class Vectorizer(object):
-    def __init__(self):
+    def __init__(self, tweets):
         pass
-    def fit_tweet(self, tweet):
-        pass
-    def finalize_fit(self):
-        pass
-    def vectorize(self, tweet):
+    def get_features(self, tweet):
         raise ValueError('Not implemented!')
 
 class TfIdfVectorizer(Vectorizer):
-    def __init__(self):
+    def __init__(self, tweets):
         self.df = Counter()
         self.tweets = 0
-    def fit_tweet(self, tweet):
-        for word in set(tweet):
-            if word not in stops:
-                self.df[word] += 1
-        self.tweets += 1
-    def finalize_fit(self):
+        for tweet in tweets:
+            for word in set(tweet):
+                if word not in stops:
+                    self.df[word] += 1
+            self.tweets += 1
         self.tweets_f = float(self.tweets)
-    def vectorize(self, tweet):
+
+    def get_features(self, tweet):
         tf = Counter(tweet)
-        return {word: tf[word] * np.log(self.tweets_f / (df[word] + 1))
+        return ['{}:{}'.format(word, tf[word] * np.log(self.tweets_f / (self.df[word] + 1)))
                 for word in tf
-                if (word not in stops and df[word] < 4 )} # I am (still) a Lisp programmer
+                if (word not in stops and self.df[word] < 4 )] # I am (still) a Lisp programmer
 
 class GloveVectorizer(Vectorizer):
-    def finalize_fit(self):
+    def __init__(self, tweets):
         self.words_dict = {}
         if args.progress:
             print 'Loading GloVe'
@@ -108,19 +106,19 @@ class GloveVectorizer(Vectorizer):
                     continue
                 self.words_dict[text[0]] = np.array([float(x) for x in text[1:]],
                                                      dtype='float32')
-    def vectorize(self, tweet):
+    def get_features(self, tweet):
         vectors = [self.words_dict[word] for word in tweet if word in self.words_dict]
         vectors.append(np.zeros(200))
         vector = np.mean(vectors, axis=0)
-        return {'_' + str(x): vector[x] for x in xrange(200)}
+        return ['_{}:{}'.format(x, vector[x]) for x in xrange(200)]
 
-
-if args.vectorizer == 'tfidf':
-    vec = TfIdfVectorizer()
-elif args.vectorizer == 'glove':
-    vec = GloveVectorizer()
-else:
-    raise ValueError('incorrect vectorizer argument: ' + repr(args.vectorizer))
+class Word2VecVectorizer(Vectorizer):
+    def __init__(self, tweets):
+        self.w2v = Word2Vec(iter(tweets), size=100, workers=cpu_count())
+    def get_features(self, tweet):
+        vectors = [self.w2v.wv[word] for word in tweet if word in self.w2v.wv] + [np.zeros(100)]
+        vector = np.mean(vectors, axis=0)
+        return ['_{}:{}'.format(x, vector[x]) for x in xrange(100)]
 
 def replace_function(*args):
     if len(args) % 2 != 0:
@@ -147,67 +145,98 @@ tweet_replace = replace_function(
 def tweet_to_words(tweet):
     return [x for x in re.split(r'[^\w<>]', tweet_replace(tweet).lower()) if x != '']
 
-def csv_rows(f, row_num=None):
-    f.seek(0)
-    for pol, id, date, lyx, user, tweet in report_progress(csv.reader(f), total=row_num):
-        if pol == '2':
-            continue
-        yield pol, id, date, lyx, user, tweet
+def generator_to_iterable(gen):
+    class Iterator(object):
+        def __init__(self, gen):
+            self.gen = gen
+        def __iter__(self):
+            return self
+        def next(self):
+            return next(self.gen)
+    class Iterable(object):
+        def __iter__(self):
+            return Iterator(gen())
+    return Iterable()
 
-if args.progress:
-    print 'Train pass [1/2]:'
-
-for pol, id, date, lyx, user, tweet in csv_rows(args.csv_file):
-    tweets += 1
-    user_tweets[user] += 1
-    if pol == '4':
-        user_positive[user] += 1
-    vec.fit_tweet(tweet_to_words(tweet))
-        
-
-ADDED_TWEETS = 1
-vec.finalize_fit()
-
-def process_file(input_file, line_visitor, train, row_num=None):
-    for pol, id, date, lyx, user, tweet in csv_rows(input_file, row_num=row_num):
-        features = []
-        lbl = 1 if pol == '4' else -1
-        pos = 1 if pol == '4' else 0
-        features.append('.user_tweets:' + str(user_tweets[user]))
-        date_match = re.match(date_re, date)
-        assert date_match is not None
-        features.append('.week_day_' + date_match.group('day'))
-        features.append('.hour_' + date_match.group('hour'))
-        tf = Counter(tweet_to_words(tweet))
-        features = vec.vectorize(tweet_to_words(tweet))
-        line_visitor.visit('{} {}|a '.format(lbl, id) + 
-            ' '.join([str(x) + ':' + str(features[x]) for x in features]))
-
-class PrintLineVisitor(object):
+class TweetsCsv(object):
     def __init__(self, file):
         self.file = file
+        self.n_tweets = None
 
-    def visit(self, line):
-        print >> self.file, line
+    def columns(self, progress_period=1000, pass_name=None):
+        def gen():
+            if args.progress:
+                if pass_name is None:
+                    print 'Passing over {} file'.format(self.file.name)
+                else:
+                    print 'Passing over {} file ({})'.format(self.file.name, pass_name)
+            tweets = 0
+            self.file.seek(0)
+            for pol, id, date, lyx, user, tweet in report_progress(csv.reader(self.file), total=self.n_tweets):
+                tweets += 1
+                if pol == '2':
+                    continue
+                pol = pol == '4'
+                date = parse_date(date)
+                tweet = tweet_to_words(tweet)
+                yield pol, id, date, lyx, user, tweet
+            self.n_tweets = tweets
+        return generator_to_iterable(gen)
 
-class StoreLineVisitor(object):
-    def __init__(self):
-        self.list = []
-    def visit(self, line):
-        self.list.append(line)
+    def tweets(self, progress_period=1000, pass_name=None):
+        def gen():
+            for pol, id, date, lyx, user, tweet in self.columns(progress_period, pass_name):
+                yield tweet
+        return generator_to_iterable(gen)
 
-if args.progress:
-    print 'Train pass [2/2]:'
-if args.write_immediately:
-    visitor = PrintLineVisitor(args.vw_file)
+    def vw_lines(self, user_tweets, user_positive, vectorizer, progress_period=1000, pass_name=None):
+        def gen():    
+            for pol, id, date, _, user, tweet in self.columns(progress_period=progress_period, pass_name=pass_name):
+                features = ['.user_tweets:' + str(user_tweets[user]),
+                            '.week_day_' + str(date.isoweekday()),
+                            '.hour_' + str(date.hour)] + vectorizer.get_features(tweet)
+                lbl = 1 if pol else -1
+
+                yield '{lbl} {tag}|x {features}'.format(
+                    lbl=(1 if pol else -1), tag=id,
+                    features=(' '.join(features)))
+        return generator_to_iterable(gen)   
+    def get_tweets_number(self):
+        if self.n_tweets is None:
+            for x in yield_everything(pass_name='counting number of lines'):
+                pass
+        return self.n_tweets
+
+train = TweetsCsv(args.csv_file)
+test = TweetsCsv(args.test_csv)
+
+for pol, id, date, lyx, user, tweet in train.columns(pass_name='getting per user info'):
+    user_tweets[user] += 1
+    if pol:
+        user_positive[user] += 1
+
+if args.vectorizer == 'tfidf':
+    vectorizer_class = TfIdfVectorizer
+elif args.vectorizer == 'glove':
+    vectorizer_class = GloveVectorizer
+elif args.vectorizer == 'w2v':
+    vectorizer_class = Word2VecVectorizer
 else:
-    visitor = StoreLineVisitor()
-process_file(args.csv_file, visitor, train=True, row_num=tweets)
-if not args.write_immediately:
-    random.shuffle(visitor.list)
-    for line in visitor.list:
-        print >> args.vw_file, line
-del visitor
-if args.progress:
-    print 'Test pass'
-process_file(args.test_csv, PrintLineVisitor(args.test_vw), train=False)
+    raise ValueError('incorrect vectorizer argument: ' + repr(args.vectorizer))
+
+
+vec = vectorizer_class(train.tweets(pass_name='training vectorization'))
+
+def print_lines_to_file(lines, file):
+    for line in lines:
+        print >> file, line
+
+if args.write_immediately:
+    print_lines_to_file(train.vw_lines(user_tweets, user_positive, vec, pass_name='train conversion'), args.vw_file)
+else:
+    lines = list(train.vw_lines(user_tweets, user_positive, vec, pass_name='train conversion'))
+    random.shuffle(lines)
+    print_lines_to_file(lines, args.vw_file)
+    del lines
+
+print_lines_to_file(test.vw_lines(user_tweets, user_positive, vec, pass_name='train conversion'), args.test_vw)
