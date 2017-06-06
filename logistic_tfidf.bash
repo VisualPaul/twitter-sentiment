60a55b1733bc2459070c9f49c390ee1ef30a1cb6
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-06-06 12:58:28 +0300

    splitted model files

--- logistic_tfidf.bash
+++ logistic_tfidf.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source venv/bin/activate
-COMMAND="vw --quiet -b 24 -d train_tfidf.vw -c --ftrl --l1 1e4 --l2 1e-4 -f model --holdout_off --loss_function logistic --link=logistic --passes 12 -q aa"
+COMMAND="vw --quiet -b 24 -d train_tfidf.vw -c --ftrl --l1 1e4 --l2 1e-4 -f model.tfidf --holdout_off --loss_function logistic --link=logistic --passes 12 -q aa"
 echo "$COMMAND"
 $COMMAND
-vw -i model -t -d test_tfidf.vw -p pred.txt --quiet
+vw -i model.tfidf -t -d test_tfidf.vw -p pred.txt --quiet
 python accuracy.py pred.txt testdata.manual.2009.06.14.csv
