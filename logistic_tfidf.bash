15c7f74bb529b51e67da6dad8d024a6d453bd985
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-10-02 13:17:33 +0000

    make logistic regression logistic again

--- logistic_tfidf.bash
+++ logistic_tfidf.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source venv/bin/activate
-COMMAND="vw --quiet -b 24 -d train_tfidf.vw -c --ftrl --l1 1e4 --l2 1e-4 -f model.tfidf --holdout_off --loss_function logistic --link=logistic --passes 12 -q aa"
+COMMAND="vw --quiet -b 24 -d train_tfidf.vw -c --ftrl --l1 1e4 --l2 1e-4 -f model.tfidf --holdout_off --loss_function logistic --link=logistic --passes 150"
 echo "$COMMAND"
 $COMMAND
 vw -i model.tfidf -t -d test_tfidf.vw -p pred.txt --quiet
