8847cec535fe48af1e2adcf1aad0d264af70c2ca
Pavel Shevchuk <iVisualPaul@gmail.com>
2017-10-02 16:09:33 +0000

    renamed model file for w2v

--- logistic_w2v.bash
+++ logistic_w2v.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source venv/bin/activate
-COMMAND="vw --quiet -b 24 -d train_w2v.vw -c --ftrl -f model --holdout_off --loss_function logistic --link=logistic --passes 10"
+COMMAND="vw --quiet -b 24 -d train_w2v.vw -c --ftrl -f model_w2v --holdout_off --loss_function logistic --link=logistic --passes 1"
 echo "$COMMAND"
 $COMMAND
-vw -i model -t -d test_w2v.vw -p pred.txt --quiet
-python accuracy.py pred.txt testdata.manual.2009.06.14.csv
+vw -i model_w2v -t -d test_w2v.vw -p pred_w2v.txt --quiet
+python accuracy.py pred_w2v.txt testdata.manual.2009.06.14.csv
